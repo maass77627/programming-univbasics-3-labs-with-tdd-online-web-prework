e012388c9174ad550db111664b2a363700e52a9a
kelly r maass <kmaass77627@gmail.com>
2019-09-23 01:17:02 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,5 @@
 first_number = 5
+<<<<<<< HEAD
 
 second_number = 2 
 
@@ -9,3 +10,6 @@ difference = first_number - second_number
 product = first_number * second_number
 
 quotient = first_number / second_number
+=======
+second_number = 2 
+>>>>>>> 0825230731cacc91a34e02884035107d2f6d85ae
